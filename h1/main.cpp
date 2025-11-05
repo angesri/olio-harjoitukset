6130487ff6f3e332a1684e07c41213ab237b4489
Riku <rikuangesleva@hotmail.com>
2025-11-05 19:10:59 +0200

    h1 valmis

---
+++ h1/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+void calcSum(int a, int b){
+
+    cout<< a << " + "<< b << " = " << a + b << endl;
+}
+
+void calcDiv(int a, int b){
+
+    if(b == 0){
+        cout<< "jakaja ei saa olla nolla!"<<endl;
+    } else
+    cout<< a << " / "<< b << " = "<< a / b << endl;
+}
+
+int retSum(int a, int b){
+
+    return a + b;
+}
+
+float retDiv(int a, int b){
+    if(b == 0){
+        throw std::runtime_error("jakaja ei saa olla nolla!");
+        }
+
+    return (float) a / b;
+}
+
+
+int main()
+{
+    int a, b;
+
+    cout << "Anna ensimmainen kokonaisluku" << endl;
+    cin>> a;
+    cout<<"Anna toinen kokonaisluku"<<endl;
+    cin>> b;
+    cout<<"Antamasi luvut ovat "<<a <<" ja "<<b <<endl;
+
+    calcSum(a,b);
+
+    calcDiv(a, b);
+
+    cout <<"retSum summa " << retSum(a, b) << endl;
+
+
+    try {
+        cout << "Osamaara " << retDiv(a, b) << endl;
+     } catch (runtime_error& e) {
+         cout<<"Error: "<< e.what()<< endl;
+     }
+
+    return 0;
+}
+
