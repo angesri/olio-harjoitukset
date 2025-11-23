be8c00d08dbfd5b82759767ba31bde3a59e21160
Riku <rikuangesleva@hotmail.com>
2025-11-23 22:36:39 +0200

    h2a korjattu, Car luotu pinoon tehtävänannon mukaisesti

--- h2a/main.cpp
+++ h2a/main.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
 int main()
 {
-    Car *objectCar1 = new Car;
-    objectCar1->setBrand("Honda");
-    objectCar1->setModel("Civic");
-    objectCar1->setYearModel(2000);
-    objectCar1->printData();
+    Car objectCar1;
+    objectCar1.setBrand("Honda");
+    objectCar1.setModel("Civic");
+    objectCar1.setYearModel(2000);
+    objectCar1.printData();
 
 
     Rectangle *objectRectangle1 = new Rectangle;
